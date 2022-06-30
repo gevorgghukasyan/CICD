20007ed556dc80b44cae8a8227b2c110ed06b0ca
Gevorg <gevorg.ghukasyan21@gmail.com>
2022-06-30 14:57:08 +0400

    mch

---
+++ CICDUnityGithub/Assets/Scripts/CICD Test.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CICDTest
+{
+    [Test]
+    public void SomeGreenTestPassing()
+    {
+        Assert.Pass();
+    }
+}
